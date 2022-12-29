77e03a2845e1d197ecf1e3d7e139fcbc9a1ea8a5
Oleg <olegdevzp@gmail.com>
2022-12-29 18:04:50 +0200

    cors

--- server.js
+++ server.js
@@ -5,13 +5,32 @@ const port = 3000;
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: './',
-    filename: function(req,file,cb){
-        cb(null,Date.now() + '.' + file.mimetype.split('/')[1])
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '.' + file.mimetype.split('/')[1])
     },
 
 })
 
-const upload = multer({storage:storage})
-app.use(cors());
-    app.post('/',upload.single('file'),(req,res) => {})
-    app.listen(port, ()=> console.log(`listening on port ${port}`));
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost',
+    'http://localhost:8080',
+    'http://localhost:8100',
+];
+
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Origin not allowed by CORS'));
+        }
+    },
+};
+
+const upload = multer({ storage: storage })
+app.use('*', cors(corsOptions));
+app.post('/', upload.single('file'), (req, res) => { })
+app.listen(port, () => console.log(`listening on port ${port}`));
